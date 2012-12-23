2b3d9f88bbbf799dbb6c4fe4bfee2eebc03187b3
ignisf <bordjukov@gmail.com>
2012-12-23 02:09:48 +0200

    Export file-locals to dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((latex-mode
+  (TeX-engine . xetex)
+  (ispell-local-dictionary . "bulgarian")
+  (TeX-master . "excercises")))
